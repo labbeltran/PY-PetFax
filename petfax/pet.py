495dbd1adfe75fb30d983ada37372bb9bc600869
labbeltran <labbeltran@gmail.com>
2024-05-09 21:16:05 -0400

    added folders to templates folders

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,9 +1,10 @@
 from flask import ( Blueprint, render_template ) 
-import json
+bp = Blueprint('pet', __name__, url_prefix="/pets")
 
+import json
 pets = json.load(open('pets.json'))
 print(pets)
-bp = Blueprint('pet', __name__, url_prefix="/pets")
+
 
 @bp.route('/')
 def index(): 
@@ -15,5 +16,5 @@ def pet_show(pet_name):
     pet = next((pet for pet in pets if pet['pet_name'] == pet_name), None)
     if pet is None:
         abort(404)  # If no pet is found, return a 404 error
-    return render_template('show_pet.html', pet=pet)
+    return render_template('pets/show_pet.html', pet=pet)
 
--- petfax/submit.py
+++ petfax/submit.py
@@ -1,6 +1,9 @@
 from flask import (Blueprint, render_template, request)
 bp= Blueprint('submit', __name__, url_prefix='/facts')
 
+@bp.route('/')
+def new():
+    return render_template('facts/index.html')
 
 @bp.route('/new', methods=('GET', 'POST'))
 def new_fact():
