5b9bff64418a2c70dddd27ef66cd6a060a382810
labbeltran <labbeltran@gmail.com>
2024-05-09 21:03:05 -0400

    submit form is not working

--- petfax/submit.py
+++ petfax/submit.py
@@ -5,12 +5,15 @@ bp= Blueprint('submit', __name__, url_prefix='/facts')
 @bp.route('/new', methods=('GET', 'POST'))
 def new_fact():
     if request.method == 'POST':
-        name = request.form.get('name')
-        pet_fact = request.form.get('pet_fact')
-        # Process form data
-        print("Name:", name)
-        print("Pet Fact:", pet_fact)
-        return 'Form submitted successfully!'
+        # name = request.form.get('name')
+        # pet_fact = request.form.get('pet_fact')
+        # # Process form data
+        # print("Name:", name)
+        # print("Pet Fact:", pet_fact)
+        # return 'Form submitted successfully!'
+        print(request.form)
+        return redirect('/facts')
+    else: return render_template('facts/new_fact.html')
     # Return a response if the request method is not POST (optional)
-    else:
-        return render_template('new_fact.html')
+    # else:
+    #     return render_template('new_fact.html')
